1803a85c70371c9e86171e559a394cd8237f6ecf
ashwani-sinha <ashwaniraj084@gmail.com>
2025-07-18 12:45:01 +0530

    updated_speaker

--- frontend/src/components/speaker/speakerdata.js
+++ frontend/src/components/speaker/speakerdata.js
@@ -2,6 +2,7 @@
 import AmitAgarwal from '../assets/speakers/Dr Amit Agarwal.png';
 import Rakesh from "../assets/speakers/Prof Dr Rakesh K Dwivedi2.png"
 import Amitkumar from "../assets/speakers/Dr Amit Kumar Bansal.png"
+import Anchit from "../assets/speakers/Anchit.png"
 
 
 const speakerData = [
@@ -10,7 +11,12 @@ const speakerData = [
     image: AmitAgarwal,
     title: <p><div>Director<br></br>ITG Gopeshwar</div></p>,
   },
-
+ {
+    name: 'Anchit Bijalwan',
+    image: Anchit,
+    title: <p><div>Associate Professor<br></br>British University Vietnam<br></br>(CGDS ARC)
+</div></p>,
+  },
   {
     name: 'Prof.(Dr.) Rakesh K Dwivedi',
     image : Rakesh,
